7efd0bc0597e62c35eb147a47ec6be88b3f473b3
akash1x <58430500+akash1x@users.noreply.github.com>
2026-02-03 11:58:32 +0530

    Time: 4 ms (35.24%) | Memory: 64.8 MB (84.77%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let minPrice = prices[0];
+    let maxProfit = 0;
+
+    for(let i=1;i<prices.length;i++){
+       maxProfit= prices[i]-minPrice > maxProfit? prices[i]-minPrice:maxProfit;
+        minPrice = prices[i]<minPrice?prices[i]:minPrice;
+    }
+    return maxProfit;
+};
