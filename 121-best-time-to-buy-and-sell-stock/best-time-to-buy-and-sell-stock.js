2944f16a90af6f7d1398a6c938885db28d96a4bf
akash1x <58430500+akash1x@users.noreply.github.com>
2026-02-03 11:59:44 +0530

    Time: 4 ms (35.24%) | Memory: 65.4 MB (58.99%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -3,12 +3,20 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let minPrice = prices[0];
-    let maxProfit = 0;
-
+    let maxProfit = 0
+    let minPrice=prices[0]
+    
     for(let i=1;i<prices.length;i++){
-       maxProfit= prices[i]-minPrice > maxProfit? prices[i]-minPrice:maxProfit;
-        minPrice = prices[i]<minPrice?prices[i]:minPrice;
+	    // Profit on ith day
+	    let profit = prices[i] - minPrice;
+    	if( profit > maxProfit){
+    		maxProfit= profit;
+    	}
+
+		//Got any price lower than current minPrice update it.
+    	if(prices[i]<minPrice){
+    		minPrice=prices[i]
+    	}
     }
-    return maxProfit;
+    return maxProfit
 };
