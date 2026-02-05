a8a4fe0c39ec905d852168cc1246ccac5c05a401
akash1x <58430500+akash1x@users.noreply.github.com>
2026-02-05 13:49:31 +0530

    Time: 50 ms (78.72%) | Memory: 59.6 MB (35.64%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.js
+++ 141-linked-list-cycle/linked-list-cycle.js
@@ -11,15 +11,13 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-    let curr = head;
-    let set = new Set();
-    while(curr){
-        if(set.has(curr)) {
-            return true;
-        }else{
-            set.add(curr);
-            curr=curr.next;
-        }
-    }
-    return false
+    if(!head) return false
+   let slow=head;
+   let fast = head.next;
+   while(slow!=fast){
+    if(!fast || !fast.next) return false;
+    slow=slow.next;
+    fast=fast.next.next;
+   }
+   return true;
 };
