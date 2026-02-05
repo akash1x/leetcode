5caf3446658b1069ea9e4d16bc62bdfbb25df092
akash1x <58430500+akash1x@users.noreply.github.com>
2026-02-05 16:50:42 +0530

    Time: 3 ms (55.87%) | Memory: 61.6 MB (51.88%) - LeetSync

---
+++ 2-add-two-numbers/add-two-numbers.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    let head = new ListNode(0)
+    let ansPointer = head
+    let carry =0;
+    while(l1 || l2 || carry){
+        let sum = (l1?l1.val:0) + (l2?l2.val:0) + carry;
+        // if sum>9 then update the carry 
+        carry = Math.floor(sum/10);
+        val = sum%10;   
+        ansPointer.next = new ListNode(val,null);
+        ansPointer=ansPointer.next;
+        l1=l1?.next
+        l2=l2?.next
+    }
+
+    return head.next;
+};
