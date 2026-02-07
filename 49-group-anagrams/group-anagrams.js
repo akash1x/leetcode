ba876ba84841b2d997f19d845fd34708fa1c7810
akash1x <58430500+akash1x@users.noreply.github.com>
2026-02-07 18:01:09 +0530

    Time: 34 ms (52.37%) | Memory: 68.5 MB (39.34%) - LeetSync

--- 49-group-anagrams/group-anagrams.js
+++ 49-group-anagrams/group-anagrams.js
@@ -3,15 +3,48 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
-    // Create a map where key will be sorted word and values will be actual words
+//     // Create a map where key will be sorted word and values will be actual words
 
-let map ={}
+// let map ={}
 
-for(let i=0;i<strs.length;i++){
-    let word = strs[i];
-    let sortedWord = word.split('').sort().join('');
-    if(map[sortedWord]) map[sortedWord].push(word);
-    else map[sortedWord] = [word];
-}
-return Object.values(map)
+// for(let i=0;i<strs.length;i++){
+//     let word = strs[i];
+//     let sortedWord = word.split('').sort().join('');
+//     if(map[sortedWord]) map[sortedWord].push(word);
+//     else map[sortedWord] = [word];
+// }
+// return Object.values(map)
+
+
+// Instead of sorted we can generate a unique key by counting char of word
+// eg: #1#4#1..
+
+    let map={}
+    //Iterate strs
+    for(let i=0;i<strs.length;i++){
+        // Create a freq array for characters 
+        let freqArr = Array(26).fill(0);
+
+        let word = strs[i];
+
+        //Update count of char in freqArr
+        for(let j=0;j<word.length;j++){
+            let charIndex= word[j].charCodeAt()-'a'.charCodeAt();
+            freqArr[charIndex]++;
+        }
+
+        //Key Generation 
+        // format : #1#4#1..
+        let key =''
+        for(let k=0;k<freqArr.length;k++){
+            
+            key+='#'+freqArr[k];
+        }
+
+        if(map[key]) map[key].push(word);
+        else map[key]=[word];
+
+    }
+
+    return Object.values(map)
 };
