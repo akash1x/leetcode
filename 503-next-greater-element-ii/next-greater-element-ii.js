1db6d27335403c1261e2dbbbc1950700763a9491
akash1x <58430500+akash1x@users.noreply.github.com>
2026-02-10 13:08:24 +0530

    Time: 6 ms (75.83%) | Memory: 62.8 MB (39.91%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.js
+++ 503-next-greater-element-ii/next-greater-element-ii.js
@@ -3,25 +3,25 @@
  * @return {number[]}
  */
 var nextGreaterElements = function(arr) {
-       let n = arr.length;
-       let ans = Array(n).fill(-1)
-    let stack = [];
-	
-	// Push last element to stack
-    stack.push(arr[n-1]);
-    
-    for(let i= (2 * n)-2; i>=0; i--){
-	    while(stack.length){
-		    let top = stack[stack.length-1];
-		    if(arr[i%n]<top){
-			    ans[i%n] = top;
-			    break;
-		    }else{
-			    stack.pop();
-		    }
-	    }
-	    stack.push(arr[i%n]);
-    
-	}
-    return ans.slice(0,n); 
-};
+  // Approach 1: Same as NGE-1 problem just double the array. 
+  // Approach 2: Iterate twice the length of array. For index use i%n
+
+  let n = arr.length;
+  let stk =[]
+  let ans= Array(n).fill(-1)
+  stk.push(arr[n-1]);
+
+  // start from 2nd last element 
+  for(let i = (2*n)-2;i>=0;i--){
+        while(stk.length){
+            if(stk[stk.length-1]>arr[i%n]){
+               ans[i%n]=stk[stk.length-1];
+                break;
+            }else{
+                stk.pop()
+            }       
+        }
+    stk.push(arr[i%n]);   
+    }
+    return ans.slice(0,n) 
+  }
