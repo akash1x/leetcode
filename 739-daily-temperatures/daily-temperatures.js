7a8f8e063703763ecaf271f984f170d2e2d3b881
akash1x <58430500+akash1x@users.noreply.github.com>
2026-02-10 12:28:28 +0530

    Time: 74 ms (5.59%) | Memory: 82.8 MB (9.34%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.js
+++ 739-daily-temperatures/daily-temperatures.js
@@ -3,25 +3,50 @@
  * @return {number[]}
  */
 var dailyTemperatures = function(arr) {
-  let n = arr.length;
-  let ans = Array(n).fill(0);
-   
-  let s=[];
-  s.push(n-1);
+// Need to calculated the distance using index difference
+// Rest it is same as NGE
 
+//Create map to store day: NGE
+let nge={}
+let stk=[]
+// Last day will never have a warmer day so 0 
+let n = arr.length
+//Index will be store as distance will be calculate by it
+stk.push(n-1);
+nge[n-1]=0;
+
+//Start from 2nd last index
 for(let i=n-2;i>=0;i--){
-    while(s.length){
-        let top= s[s.length-1];
-        if(arr[i]>=arr[top]){
-            s.pop();
-        }else{
-            ans[i]=top-i;
-            break;
+    // Check stack top > current day then top is the warmer day
+    // Store index difference
+    if(arr[stk[stk.length-1]]>arr[i]){
+        nge[i]=stk[stk.length-1] - i;
+        //push the current day as it can warmer for other days
+        stk.push(i)
+    }else{
+        // Keep popping till we find the NGE
+        while(stk.length){
+            if(arr[stk[stk.length-1]]<=arr[i]){
+                stk.pop();
+            }else{
+                nge[i]=stk[stk.length-1] - i;
+                //push the current day as it can warmer for other days
+                stk.push(i);
+                break;
+            }
+        }
+        //No NGE found
+        if(stk.length===0){
+            nge[i]=0
+            stk.push(i);
         }
     }
-    s.push(i)
+}
+// Now in nge we have map of day: nextWarmerDay
+let ans =[];
+for(let i=0;i<arr.length;i++){
+    ans.push(nge[i]);
 }
 
-return ans
-
+return ans;
 };
