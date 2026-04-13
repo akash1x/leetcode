daf2428936e8dfe7fbe36fde95ced33f1caa487d
akash1x <58430500+akash1x@users.noreply.github.com>
2026-04-13 11:30:39 +0530

    Time: 1 ms (59.86%) | Memory: 61.1 MB (63.16%) - LeetSync

--- 344-reverse-string/reverse-string.js
+++ 344-reverse-string/reverse-string.js
@@ -3,12 +3,10 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    let ePointer = s.length-1;
-    for(let sPointer=0;sPointer<Math.floor(s.length/2);sPointer++){
-        let temp = s[sPointer];
-        s[sPointer]= s[ePointer];
-        s[ePointer]= temp;
-        ePointer--;
+    for(let i=0;i<Math.floor(s.length/2);i++){
+        let temp = s[i];
+        s[i]=s[s.length-1-i];
+        s[s.length-1-i]=temp;
     }
-    return s
+    return s;
 };
